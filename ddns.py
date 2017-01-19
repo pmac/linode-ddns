8c470eed947017cd02a766c9a723650158f7ec82
Paul McLanahan <pmac@mozilla.com>
2017-01-18 22:17:36 -0500

    Require an API Key

--- ddns.py
+++ ddns.py
@@ -1,11 +1,16 @@
-from flask import Flask, request
+import os
+from flask import Flask, abort, request
 from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
+API_KEY = os.environ['API_KEY']
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def home():
-    return request.environ['REMOTE_ADDR']
+    if request.form['api_key'] == API_KEY:
+        return request.environ['REMOTE_ADDR']
+
+    abort(400)
